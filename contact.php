c59ff70ea557f10004e83e6bd7e6d2b8151dd2a1
asthikambalapadi <asthikpb@gmail.com>
2020-06-08 18:09:52 +0530

    PHP file updated

--- contact.php
+++ contact.php
@@ -1,15 +1,18 @@
 <?php
 $name = $_POST['name'];
-$mail = $_POST['mail'];
+$visitor_email = $_POST['email'];
 $message = $_POST['message'];
-$email_form = 'asthikpb@gmail.com';
-$email_sub = "Contact Us";
-$email_body = "Name: $name \n".
-              "Mail ID: $mail \n".
-              "Message: $message \n";
-$headers = "From: $email_form \r\n";
-$headers = "Reply to: $mail \r\n";
-$to = "asthikpbce@gmail.com";
-mail ($to,$email_sub,$message,$headers)
-header ("Location: index.html");
+
+$email_from = 'asthikpb@gmail.com';
+$email_subject = 'Contact Us';
+$email_body = 'User name: $name.\n'.
+              'User email: $email.\n'.
+              'User message: $message.\n';
+
+$to = 'asthikpbce@gmail.com';
+$headers = 'From: $email_from \r\n';
+$headers = 'Reply-to: $visitor_email \r\n';
+
+mail ($to,$email_subject,$message,$headers)
+header ('Location: index.html');
 ?>
